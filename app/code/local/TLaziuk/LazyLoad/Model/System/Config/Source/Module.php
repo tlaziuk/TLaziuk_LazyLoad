57d6cccc0e89a515859b8802d97058cea20dbeb8
Tomek Łaziuk <tlaziuk@gmail.com>
2016-12-19 00:40:53 +0100

    show only modules with blocks

--- app/code/local/TLaziuk/LazyLoad/Model/System/Config/Source/Module.php
+++ app/code/local/TLaziuk/LazyLoad/Model/System/Config/Source/Module.php
@@ -8,9 +8,24 @@ class TLaziuk_LazyLoad_Model_System_Config_Source_Module
     {
         if (empty($this->_options)) {
             $config = Mage::app()->getConfig();
-            $modules = (array)$config->getNode('modules')->asArray();
-            ksort($modules);
-            foreach ($modules as $name => $data) {
+            $modules = array_keys((array) $config->getNode('modules')->asArray());
+            $blocks = array();
+            foreach ((array) $config->getNode('global')->xpath('blocks//class') as $block) {
+                $blocks = array_merge($blocks, (array) $block);
+            }
+            foreach ((array) $config->getNode('global')->xpath('blocks//rewrite') as $rewrite) {
+                $blocks = array_merge($blocks, array_values($rewrite->asArray()));
+            }
+            $modules = array_filter($modules, function($value) use (&$blocks) {
+                foreach ($blocks as $block) {
+                    if (strpos($block, $value) === 0) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+            sort($modules);
+            foreach ($modules as $name) {
                 $this->_options[] = array(
                     'value' => $name,
                     'label' => $name,
