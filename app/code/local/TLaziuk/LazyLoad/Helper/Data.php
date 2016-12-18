2f403f76650bcba8a2ca82bd668d6c23e209d7f2
Tomek Łaziuk <tlaziuk@gmail.com>
2016-12-18 16:44:11 +0100

    cache

--- app/code/local/TLaziuk/LazyLoad/Helper/Data.php
+++ app/code/local/TLaziuk/LazyLoad/Helper/Data.php
@@ -6,6 +6,25 @@ class TLaziuk_LazyLoad_Helper_Data extends Mage_Core_Helper_Abstract
     const XML_PATH_ALL = 'advanced/tlaziuk_lazyload/all';
     const XML_PATH_ATTRIBUTE = 'advanced/tlaziuk_lazyload/attribute';
 
+    const CACHE_TAG = 'TLaziuk_LazyLoad';
+    const CACHE_GROUP = Mage_Core_Block_Abstract::CACHE_GROUP;
+    const CACHE_LIFETIME = Mage_Core_Model_Cache::DEFAULT_LIFETIME;
+    const CACHE_KEY_PREFIX = self::CACHE_TAG;
+
+    public function loadCache($key) {
+        if (Mage::app()->useCache(self::CACHE_GROUP)) {
+            return Mage::app()->loadCache($key);
+        }
+        return false;
+    }
+
+    public function saveCache($data, $key, array $tags = array(self::CACHE_TAG), $lifetime = self::CACHE_LIFETIME) {
+        if (Mage::app()->useCache(self::CACHE_GROUP)) {
+            Mage::app()->saveCache($data, $key, $tags, $lifetime);
+        }
+        return $this;
+    }
+
     protected $_attribute;
 
     public function getAttribute() {
@@ -90,25 +109,31 @@ class TLaziuk_LazyLoad_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function lazyBlock(Mage_Core_Block_Abstract $block, Varien_Object $transport) {
         try {
-            $name = $block->getModuleName() . '::' . get_class($block) . '::' . ($block->getNameInLayout() ? $block->getNameInLayout() : 'anonymous') . '::' . spl_object_hash($block);
+            $name = $block->getModuleName() . '::' . get_class($block) . '::' . ($block->getNameInLayout() ? $block->getNameInLayout() : 'anonymous');
             Varien_Profiler::start('TLaziuk_LazyLoad::lazyBlock::' . $name);
-            $html = $transport->getHtml();
-            $html = preg_replace_callback($this->_commentPattern, function($matches) {
-                if (!empty($matches[2])) {
-                    $this->_tmpHtml[$matches[1]] = $matches[2];
-                    return $matches[1] . $matches[1];
+            $key = self::CACHE_KEY_PREFIX . '::' . $name;
+            if (!($html = $this->loadCache($key))) {
+                $html = $transport->getHtml();
+                $html = preg_replace_callback($this->_commentPattern, function($matches) {
+                    if (!empty($matches[2])) {
+                        $this->_tmpHtml[$matches[1]] = $matches[2];
+                        return $matches[1] . $matches[1];
+                    }
+                    return '';
+                }, $html);
+                if (in_array($block->getModuleName(), $this->getModule())) {
+                    $html = $this->lazyHtml($html);
                 }
-                return '';
-            }, $html);
-            if (in_array($block->getModuleName(), $this->getModule())) {
-                $html = $this->lazyHtml($html);
+                $html = preg_replace_callback($this->_commentPattern, function($matches) {
+                    if (empty($matches[2]) && !empty($this->_tmpHtml[$matches[1]])) {
+                        return $this->_tmpHtml[$matches[1]];
+                    }
+                    return $matches[2];
+                }, $html);
+                $this->saveCache($html, $key);
+            } else {
+                $this->_tmpHtml["<!--{$name}-->"] = $html;
             }
-            $html = preg_replace_callback($this->_commentPattern, function($matches) {
-                if (empty($matches[2]) && !empty($this->_tmpHtml[$matches[1]])) {
-                    return $this->_tmpHtml[$matches[1]];
-                }
-                return $matches[2];
-            }, $html);
             if ($block->getParentBlock()) {
                 $html = "<!--{$name}-->{$html}<!--{$name}-->";
             }
@@ -125,7 +150,12 @@ class TLaziuk_LazyLoad_Helper_Data extends Mage_Core_Helper_Abstract
         foreach ($response->getBody(true) as $name => $html) {
             Varien_Profiler::start('TLaziuk_LazyLoad::lazyResponse::'.$name);
             try {
-                $response->setBody($this->lazyHtml($html), $name);
+                $key = self::CACHE_KEY_PREFIX . '::' . 'response' . '::' . $name;
+                if (!($lazy = $this->loadCache($key))) {
+                    $lazy = $this->lazyHtml($html);
+                    $this->saveCache($lazy, $key);
+                }
+                $response->setBody($lazy, $name);
             } catch (Exception $e) {
                 Mage::logException($e);
             }
