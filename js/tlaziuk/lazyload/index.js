82931e8f1c5f46155184938606d31ccc27084d36
Tomek Łaziuk <tlaziuk@gmail.com>
2017-03-30 15:32:52 +0200

    do not skip invisible images

--- js/tlaziuk/lazyload/index.js
+++ js/tlaziuk/lazyload/index.js
@@ -2,7 +2,9 @@
 (function() {
     document.addEventListener('DOMContentLoaded', function(evt) {
         try {
-            new LazyLoad();
+            new LazyLoad({
+                skip_invisible: false,
+            });
         } catch (e) {
             console.error(e);
         }
